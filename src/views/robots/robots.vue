6158b59761063db5269711c9f5c0f7e876458be0
Vergil <157215961@qq.com>
2023-11-28 17:29:27 +0800

    123

--- src/views/robots/robots.vue
+++ src/views/robots/robots.vue
@@ -2,12 +2,21 @@
   <el-form :inline="true" :model="formInline" class="demo-form-inline">
     <el-container>
       <el-main>
-        <div ref="chatbox" class="chatbox">
+
+
+        <!-- <div ref="chatbox" class="chatbox">
           <div v-for="message in messages" :key="message.id" class="message">
             {{ message.content }}
           </div>
           <input v-model="newMessage" type="text" placeholder="请输入消息" @keyup.enter="sendMessage">
+        </div> -->
+        <div ref="chatbox">
+          <p v-for="(item, index) in concatenatedString">{{ item }}</p>
         </div>
+
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+        <el-button type="primary" @click="sendmsg"></el-button>
+
 
       </el-main>
     </el-container>
@@ -18,6 +27,8 @@
 import axios from 'axios'
 import { constants } from 'fs'
 
+
+
 export default {
   data() {
     return {
@@ -26,17 +37,30 @@ export default {
         user: '',
         region: ''
       },
-
-      messages: [
-        { id: 1, content: 'Hello' },
-        { id: 2, content: 'Hi' }
-      ],
-      newMessage: '',
-      theanswer: ''
+      concatenatedString: ['机器人的回答：'],
+      input: '',
+      websocket: null,
     }
   },
 
   created() {
+    this.websocket = new WebSocket('ws://172.20.200.121:8090'); // 替换为你的WebSocket地址
+    this.websocket.onmessage = (event) => {
+      let data = event.data
+      // 在接收到事件时，将字符串拼接到已有的字符串上
+      if (data.indexOf('\n\n') !== -1 || data.indexOf('\n') !== -1) {
+        const list = data.split(/\n\n|\n/);
+        let totallength = this.concatenatedString.length - 1
+        this.concatenatedString[totallength] += list[0]
+        for (let i = 1; i < list.length; i++) {
+          this.concatenatedString.push(list[i])
+        }
+      }
+      else {
+        let totallength = this.concatenatedString.length - 1
+        this.concatenatedString[totallength] += data
+      }
+    };
   },
   mounted() {
     // 滚动到最新消息
@@ -45,30 +69,11 @@ export default {
     })
   },
 
-  methods: {
-    sendMessage() {
-      if (this.newMessage.trim()) {
-        this.messages.push({
-          id: Date.now(),
-          content: this.newMessage.trim()
-        })
-        axios.post(process.env.VUE_APP_CONFIG_API + '/gpt/chat/getanswer', {
-          question: this.newMessage.trim()
-        })
-          .then(response => {
-            this.theanswer = response.data
-            this.messages.push({
-              id: Date.now(),
-              content: this.theanswer.trim()
-            })
-          })
-          .catch(error => {
-            // 处理错误
-          })
-        this.newMessage = ''
-      }
-    }
 
+  methods: {
+    sendmsg() {
+      this.websocket.send(JSON.stringify(this.input))
+    }
   }
 }
 </script>
